21ae3f30acd0d4aa28894bb1e150aa5994d194c0
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-23 13:47:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,13 +8,13 @@ end
 
 def valid_move?(board, index)
   if (index.between?(0, 8)) && !(position_taken?(board, index))
-    return true
+    true
   end
 end
 
 def position_taken?(board, index)
   if (board[index] != " " || board[index] != "")
-    return true
+    true
   end
 end
 
