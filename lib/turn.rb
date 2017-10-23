121aa9d354ce45700637b6d90213ebdc8ec28c1e
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-23 13:49:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,20 +6,24 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  input.to_i - 1
+end
+
 def valid_move?(board, index)
   if (index.between?(0, 8)) && !(position_taken?(board, index))
     true
+  else
+    false
   end
 end
 
 def position_taken?(board, index)
   if (board[index] != " " || board[index] != "")
     true
+  else
+    false
   end
-end
-
-def input_to_index(input)
-  input.to_i - 1
 end
 
 def move(board, index, marker="X")
