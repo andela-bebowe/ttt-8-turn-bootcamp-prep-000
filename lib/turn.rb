a11fc54a6e783eb1853be286e01295716af058f5
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-23 13:33:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,10 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, index, position = "X")
+def valid_move?(board, index)
 
 end
 
-def position_taken
+def position_taken?()
 
 end
